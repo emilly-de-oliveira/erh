95821ee976a9ffd985b63f3e61ed9b27ded2a785
emilly-de-oliveira <113993018+emilly-de-oliveira@users.noreply.github.com>
2023-05-24 21:13:09 -0300

    index.php

--- exercicioPhp/index.php
+++ exercicioPhp/index.php
@@ -1,7 +1,7 @@
 <?php
 include "paginas/estrutura/cabecalho.inc.php";
 include "paginas/estrutura/menu.inc.php";
-include "paginas/ContatoForm.php";
-include "paginas/ContatoList.php";
+include "paginas/MusicaForm.php";
+include "paginas/MusicaList.php";
 include "paginas/estrutura/rodape.inc.php";
 ?>
