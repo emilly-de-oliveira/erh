8cb56ce717ad74b9d1435d970aa8150429240eb3
emilly-de-oliveira <113993018+emilly-de-oliveira@users.noreply.github.com>
2023-05-24 21:26:17 -0300

    EX.class.php

--- exercicioPhp/EX.class.php
+++ exercicioPhp/EX.class.php
@@ -2,43 +2,42 @@
 
 class EX {
     private $host = "localhost";
-    private $xename = "xe_aula";
+    private $exname = "ex_aula";
     private $port = 3306;
-    private $usuario = "root";
-    private $senha = "";
-    private $xe_charset = "utf8";
+    private $musica = "root";
+    private $ex_charset = "utf8";
 
 
     public function conn(){
-        $conn = "mysql:host=$this->host;xename=$this->xename;port=$this->port";
+        $conn = "mysql:host=$this->host;exname=$this->exname;port=$this->port";
 
         return new PDO(
             $conn,
-            $this->usuario,
-            $this->senha,
-            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ". $this->xe_charset]
+            $this->musica,
+
+            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ". $this->ex_charset]
         );
     }
 
     public function inserir($dados){
         $conn = $this->conn();
-        $sql = "INSERT INTO usuario (nome, email, telefone) VALUES (?, ?, ?);";
+        $sql = "INSERT INTO musica (nome, cantor, data) VALUES (?, ?, ?);";
         $st = $conn->prepare($sql);
-        $st->execute([$dados['nome'], $dados['email'],$dados['telefone']]);
+        $st->execute([$dados['nome'], $dados['cantor'],$dados['data']]);
     }
 
     public function atualizar($dados){
 
         $id = $dados['id'];
         $conn = $this->conn();
-        $sql = "UPDATE usuario SET nome=?, email=?, telefone=? WHERE id = $id ";
+        $sql = "UPDATE musica SET nome=?, cantor=?, data=? WHERE id = $id ";
         $st = $conn->prepare($sql);
-        $st->execute([$dados['nome'], $dados['email'],$dados['telefone']]);
+        $st->execute([$dados['nome'], $dados['cantor'],$dados['data']]);
     }
 
     public function select(){
         $conn = $this->conn();
-        $sql = "SELECT * FROM usuario;";
+        $sql = "SELECT * FROM musica;";
         $st = $conn->prepare($sql);
         $st->execute();
 
@@ -47,7 +46,7 @@ class EX {
 
     public function buscar($id){
         $conn = $this->conn();
-        $sql = "SELECT * FROM usuario WHERE id=?;";
+        $sql = "SELECT * FROM musica WHERE id=?;";
         $st = $conn->prepare($sql);
         $st->execute([$id]);
 
@@ -56,7 +55,9 @@ class EX {
 
     public function deletar($id){
         $conn = $this->conn();
-        $sql = "DELETE FROM usuario WHERE id = ?";
+        $sql = "DELETE FROM musica
+
+WHERE id = ?";
         $st = $conn->prepare($sql);
         $st->execute([$id]);
     }
